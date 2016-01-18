e4952fd58ae93676d8025ef6ce6e8acc52f84faf
David Deng <daviddengcn@gmail.com>
2016-01-18 08:59:15 -0800

    Add testcase(coverage=100% now). Add MaxStackDepth.

--- errors.go
+++ errors.go
@@ -1,4 +1,4 @@
-package errors
+package errorsp
 
 import (
 	"fmt"
@@ -6,6 +6,13 @@ import (
 	"runtime"
 )
 
+var (
+	// The maximum depth in ErrorWithStacks.Stacks.
+	// The last line is set to "..." if some call stacks are ignored.
+	MaxStackDepth int = 10
+)
+
+// ErrorWithStacks is a struct containing the original error and the call stacks.
 type ErrorWithStacks struct {
 	Err    error
 	Stacks []string
@@ -13,6 +20,7 @@ type ErrorWithStacks struct {
 
 var _ error = (*ErrorWithStacks)(nil)
 
+// Error implements the error interface.
 func (e *ErrorWithStacks) Error() string {
 	msg := e.Err.Error()
 	for _, s := range e.Stacks {
@@ -21,20 +29,32 @@ func (e *ErrorWithStacks) Error() string {
 	return msg
 }
 
+// WithStacks returns a *ErrorWithStacks error with stacks set.
+// If err has been a *ErrorWithStacks, it is directly returned.
+// If err is nil, a nil is returned.
 func WithStacks(err error) error {
 	if err == nil {
+		// Remain no-error.
 		return nil
+	}
+	if _, ok := err.(*ErrorWithStacks); ok {
+		// If err has been a ErrorWithStacks, no need to wrap it.
+		return err
 	}
 	e := &ErrorWithStacks{
 		Err: err,
 	}
-	for i := 0; i < 10; i++ {
+	for i := 0; i <= MaxStackDepth; i++ {
 		_, file, line, ok := runtime.Caller(i + 1)
 		if !ok {
 			break
 		}
 
-		e.Stacks = append(e.Stacks, fmt.Sprintf("%s:%d", path.Base(file), line))
+		if i >= MaxStackDepth {
+			e.Stacks = append(e.Stacks, "...")
+		} else {
+			e.Stacks = append(e.Stacks, fmt.Sprintf("%s:%d", path.Base(file), line))
+		}
 	}
 	return e
 }
---
+++ errors_test.go
@@ -0,0 +1,32 @@
+package errorsp
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/golangplus/testing/assert"
+)
+
+func TestWithStacks_NoError(t *testing.T) {
+	assert.Equal(t, "WithStacks", WithStacks(nil), nil)
+}
+
+func TestWithStacks(t *testing.T) {
+	origin := fmt.Errorf("myerror")
+	wrapped, isErrWithStacks := WithStacks(origin).(*ErrorWithStacks)
+	assert.True(t, "isErrWithStacks", isErrWithStacks)
+	assert.Equal(t, "wrapped.Err", wrapped.Err, origin)
+
+	wrapped2, isErrWithStacks := WithStacks(wrapped).(*ErrorWithStacks)
+	assert.True(t, "isErrWithStacks", isErrWithStacks)
+	assert.Equal(t, "wrapped2", wrapped2, wrapped)
+}
+
+func TestMaxStackDepth(t *testing.T) {
+	MaxStackDepth = 1
+	err := WithStacks(fmt.Errorf("myerror")).(*ErrorWithStacks)
+	assert.Equal(t, "len(err.Stacks)", len(err.Stacks), 2)
+	assert.Equal(t, "err.Stacks[1]", err.Stacks[1], "...")
+	assert.Equal(t, "lines of err.Error()", len(strings.Split(err.Error(), "\n")), 3)
+}
